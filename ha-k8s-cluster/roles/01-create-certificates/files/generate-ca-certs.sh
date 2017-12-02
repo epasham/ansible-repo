ee45b7260908e3ed5994d4046b05400245b38543
Ekambaram Pasham <epasham@users.noreply.github.com>
2017-12-02 14:20:25 +0530

    Update generate-ca-certs.sh

--- ha-k8s-cluster/roles/01-create-certificates/files/generate-ca-certs.sh
+++ ha-k8s-cluster/roles/01-create-certificates/files/generate-ca-certs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Generate the CA certificate and private key
-cd /k8s/certs/; cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+cd /k8s/certs/; /usr/local/bin/cfssl gencert -initca ca-csr.json | /usr/local/bin/cfssljson -bare ca
